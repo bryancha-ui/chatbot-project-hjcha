e1152664ad9da8aca7bfb6380558bf5296493ed7
limhanyong <nineifrst77@gmail.com>
2022-08-22 16:23:32 +0900

    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 ## DB 연결 Local
 def db_create():
     #로컬
-    engine = create_engine("postgresql://postgres:12232305@localhost:5432/postgres",echo=False)
+    engine = create_engine("postgresql://cnydwzldgtmgee:14146368f07d7868e027f3097f6f54876788e37dd531eb365d80d665682f070d@localhost:5432/d3s0cr9g4rtoc1",echo=False)
     #postgresql://username:password@localhost:5432/Maintenance database
     #Heroku
     # engine = create_engine("postgresql://izqesmsjoczkzz:dded6e1737ec332ef2bb0a898d23101b70406d9e25cb44fd472079b9a2aab111@ec2-54-225-234-165.compute-1.amazonaws.com:5432/dbqh4fnj6la4kq", echo = False)
